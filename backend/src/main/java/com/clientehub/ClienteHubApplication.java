44f39b40e4a6850c9afee7c583b2e8d627346be8
Reinaldo Barreto da Silva <92860945+reinaldobarreto31@users.noreply.github.com>
2026-07-30 05:51:51 -0300

    feat: add backend/src/main/java/com/clientehub/ClienteHubApplication.java

---
+++ backend/src/main/java/com/clientehub/ClienteHubApplication.java
@@ -0,0 +1,11 @@
+package com.clientehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteHubApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClienteHubApplication.class, args);
+    }
+}
